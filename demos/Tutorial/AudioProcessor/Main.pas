72bd346fb4eb7e0d8924bce678dfec665b31b1e4
wayne@wootcomputers.com <wayne@wootcomputers.com@84581c87-d242-0410-b61f-61fc836ab3ff>
2008-01-05 19:21:02 +0000

    - Updated comments in demos/Tutorial/AudioProcessor/Main.pas

--- demos/Tutorial/AudioProcessor/Main.pas
+++ demos/Tutorial/AudioProcessor/Main.pas
@@ -1,13 +1,16 @@
-(*
-   This is AudioProcessor demo which many people asked me about.
-   This demo acceps 16 bps input. If the input is mono it is left as is,
-   if the input is stereo it is converted to mono.
-   In order to use AudioProcessor component you should set its OnGeData event
-   handler at least.
-   In this demo we also set OnGetChannels event handler
-   Don't forget to assign WaveIn1's FileName property.
-   (c) Andrei Borovsky, anb@symmetrica.net
-*)
+{ Title: Audio Processor
+    This demo converts stereo to mono. If the input is mono it is left as is, if 
+    the input is stereo it is converted to mono. 
+
+    - This demo accepts 16 bps input. 
+    - In order to use AudioProcessor component you should set its OnGeData event
+      handler at least.
+    - In this demo we also set OnGetChannels event handler.
+    -  Don't forget to assign WaveIn1's FileName property.
+   
+}
+
+{ (c) Andrei Borovsky, anb@symmetrica.net }
 
 unit Main;
 
@@ -56,7 +59,9 @@ implementation
 {$R *.dfm}
 
 (* Here Data is the poiner to the processor output data,
-n - data length in bytes *)
+n - data length in bytes 
+
+What does this mean? - Wayne *)
 
 procedure TForm1.AudioProcessor1GetChannels(Sender: TComponent;
   var Param: Integer);
@@ -79,7 +84,8 @@ begin
   Button1.Enabled := True;
 end;
 
-// This OnClose event prevents exceptions if he form is cosed while playing
+{ Procedure: FormClose
+    This OnClose event prevents exceptions if he form is cosed while playing. }
 procedure TForm1.FormClose(Sender: TObject; var Action: TCloseAction);
 begin
   DXAudioOut1.Stop;
@@ -91,17 +97,25 @@ end;
 procedure TForm1.AudioProcessor1GetData(Sender: TComponent;
   var Buffer: Pointer; var Bytes: Integer);
 var
-  // We assume the input is 16 bits per sample
+  { Variables: Buffers
+      We assume the input is 16 bits per sample.
+      
+    PS16: PStereoBuffer16 - pointer to a buffer for stereo data
+    P16: PBuffer16 - pointer to a buffer for mono data 
+  }
+   
   PS16 : PStereoBuffer16;
   P16 : PBuffer16;
   i, DataSize : Integer;
   b : Boolean;
 begin
-  // Call to Input.GetData(Buffer, Bytes)
-  //  retrieves a part of the input component's buffer (pointed to by Buffer)
-  // that contains The number of bytes returned in Bytes.
-  // See the Component Writers Guide on more detail about how GetData() operates.
-
+  { Topic: Get Data
+      A all to Input.GetData(Buffer, Bytes) retrieves a part of the input 
+      component's buffer (pointed to by Buffer) that contains The number of 
+      bytes returned in Bytes.      
+      
+      See the <Component Writer's Guide> on more detail about how GetData() operates.
+  }
   // if the input is mono we simply pass data through...
   if AudioProcessor1.Input.Channels = 1 then
     AudioProcessor1.Input.GetData(Buffer, Bytes)
@@ -117,9 +131,10 @@ begin
     end;
     DataSize := Bytes * 2;
     if DataSize > TmpBufSize then DataSize := TmpBufSize;
-    // EndOfInput is set to True when FillBuffer has encountered the end of data
-    // in AudioProcessor1.Input. There still may be some data in the input,
-    // but the next time this method is called we should return nil.
+    { Topic: FillBuffer
+        EndOfInput is set to True when FillBuffer has encountered the end of data
+        in AudioProcessor1.Input. There still may be some data in the input,
+        but the next time this method is called we should return nil. }
     DataSize := AudioProcessor1.Input.FillBuffer(@TmpBuf[0], DataSize, EndOfInput);
     P16 := @TmpBuf[0];
     PS16 := @TmpBuf[0];
