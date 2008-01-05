c22e11261f9819729f24bd27c28242ee5929afa7
wayne@wootcomputers.com <wayne@wootcomputers.com@84581c87-d242-0410-b61f-61fc836ab3ff>
2008-01-05 05:20:46 +0000

    - added documentation into gendoc.bat explaining why it is like it is  - generated documentation for demos

--- doc/gendoc.bat
+++ doc/gendoc.bat
@@ -1,1 +1,13 @@
+rem this ends up being a call to a batch file outside the project tree
+rem that's because it contains passwords to my slave box where NaturalDocs lives
+rem on the slave, v: is mapped to "d:\projects" on the primary computer.
+rem 
+rem if you're not familiar with psexec, it's part of pstools, ask google.
+rem
+rem gendoc_newac.bat looks like this:
+rem
+rem copy d:\projects\newac\doc\config\newac.css \\slave\d$\naturaldocs\styles
+rem psexec \\slave -u user -p pass d:\naturaldocs\nd.bat -s newac -img v:\newac\doc\images\ -i v:\newac\ -o html v:\newac\doc\html -p v:\newac\doc\config
+
 call d:\projects\gendoc_newac.bat
+
