1e5096c7773237d8b9d2c760592e010968cbdd6c
wayne@wootcomputers.com <wayne@wootcomputers.com@84581c87-d242-0410-b61f-61fc836ab3ff>
2008-01-05 23:32:55 +0000

    - Fixed spelling errors in demos/Tutorial/AudioProcessor/Main.pas

--- trunk/demos/Tutorial/AudioProcessor/Main.pas
+++ trunk/demos/Tutorial/AudioProcessor/Main.pas
@@ -1,10 +1,9 @@
-
 (* Title: Audio Processor
-    This demo converts stereo to mono. If the input is mono it is left as is, if
-    the input is stereo it is converted to mono.
+    This demo converts stereo to mono. If the input is mono it is left as is, if 
+    the input is stereo it is converted to mono. 
 
-    - This demo accepts 16 bps input.
-    - In order to use AudioProcessor component you should set its OnGetData event
+    - This demo accepts 16 bps input. 
+    - In order to use AudioProcessor component you should set its OnGeData event
       handler at least.
     - In this demo we also set OnGetChannels event handler.
     -  Don't forget to assign WaveIn1's FileName property. *)
@@ -57,7 +56,7 @@ implementation
 
 {$R *.dfm}
 
-(* Here Data is the poiner to the processor output data,
+(* Here Data is the pointer to the processor output data,
 n - data length in bytes 
 
 What does this mean? - Wayne *)
@@ -84,7 +83,7 @@ begin
 end;
 
 (* Procedure: FormClose
-    This OnClose event prevents exceptions if he form is cosed while playing. *)
+    This OnClose event prevents exceptions if he form is closed while playing. *)
 procedure TForm1.FormClose(Sender: TObject; var Action: TCloseAction);
 begin
   DXAudioOut1.Stop;
@@ -100,21 +99,19 @@ var
       We assume the input is 16 bits per sample.
       
     PS16: PStereoBuffer16 - pointer to a buffer for stereo data
-    P16: PBuffer16 - pointer to a buffer for mono data 
-  *)
+    P16: PBuffer16 - pointer to a buffer for mono data. *)
    
   PS16 : PStereoBuffer16;
   P16 : PBuffer16;
   i, DataSize : Integer;
   b : Boolean;
 begin
-  (* Topic: GetData
-      A call to Input.GetData(Buffer, Bytes) retrieves a part of the input 
-      component's buffer (pointed to by Buffer) that contains The number of 
-      bytes returned in Bytes.      
+  (* Topic: Get Data
+      A all to Input.GetData(Buffer, Bytes) retrieves a part of the input
+      component's buffer (pointed to by Buffer) that contains The number of
+      bytes returned in Bytes.
       
-      See the <Component Writer's Guide> on more detail about how GetData operates.
-  *)
+      See the <Component Writer's Guide> on more detail about how GetData() operates. *)
   // if the input is mono we simply pass data through...
   if AudioProcessor1.Input.Channels = 1 then
     AudioProcessor1.Input.GetData(Buffer, Bytes)
@@ -131,9 +128,10 @@ begin
     DataSize := Bytes * 2;
     if DataSize > TmpBufSize then DataSize := TmpBufSize;
     (* Topic: FillBuffer
-        EndOfInput is set to True when FillBuffer has encountered the end of data
-        in AudioProcessor1.Input. There still may be some data in the input,
-        but the next time this method is called we should return nil. *)
+        EndOfInput is set to True when FillBuffer has encountered the end of
+        data in AudioProcessor1.Input. There still may be some data in the
+        input, but the next time this method is called we should return nil.
+        *)
     DataSize := AudioProcessor1.Input.FillBuffer(@TmpBuf[0], DataSize, EndOfInput);
     P16 := @TmpBuf[0];
     PS16 := @TmpBuf[0];
