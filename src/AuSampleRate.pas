6135e357e4fea9e6f6a76a5695d81ad4b2b5a476
wayne@wootcomputers.com <wayne@wootcomputers.com@84581c87-d242-0410-b61f-61fc836ab3ff>
2008-04-06 16:37:28 +0000

    Fixed comments.

--- src/AuSampleRate.pas
+++ src/AuSampleRate.pas
@@ -25,6 +25,7 @@ const
 
 type
 
+
   TResamplerQuality = (rqBest, rqMedium, rqFastest);
 
   (* Class: TResampler
@@ -63,26 +64,36 @@ type
     destructor Destroy; override;
   published
     (* Property: Quality
-       Use this property to set the trade-off between resampling quality and speed.
-       Possible values are rqBest, rqMedium, and rqFastest, of which rqBest sets the best quality but slow resampling time
-       and rqFastest sets the lower (but still very good) quality and the fastest resampling time.*)
+       Use this property to set the trade-off between resampling quality and
+       speed. Possible values are <rqBest>, <rqMedium>, and <rqFastest>, of
+       which rqBest sets the best quality but slow resampling time and
+       rqFastest sets the lower (but still very good) quality and the fastest
+       resampling time. *)
     property Quality : TResamplerQuality read FQuality write FQuality;
     (* Property: OutSampleRate
       Use this property to set the sample rate for the resulting audio stream.
-      The valid values range from 2000 to 120000 and include special value of 0.
-      If you set the output sample rate to the same value as the input sample rate, or set it to 0, the component will switch
-      to a pass-through mode. In this mode all the input will be passed on unchanged. This feature is useful when chaining the TResampler component wih other
-      components in the chain that may not always need resampled data.*)
+      Valid values range from 2000 to 120000 and include the special value of
+      0. If you set the output sample rate to the same value as the input
+      sample rate, or set it to 0, the component will switch to a pass-through
+      mode. In this mode all the input will be passed on unchanged. This
+      feature is useful when chaining the TResampler component wih other
+      components in the chain that may not always need resampled data. *)
     property OutSampleRate : LongWord read FOutSampleRate write SetOutSampleRate;
   end;
 
-
+  (* Enum: TResamplerQuality
+    
+      rqBest - High quality, but slowest resampling time.
+      rqMedium - Medium quality
+      rqFastest - Lowest quality, but still not too bad. Also the fastest resampling time.
+  *)
+  
 implementation
 
   constructor TResampler.Create;
   begin
     inherited Create(AOwner);
-    if not (csDesigning	in ComponentState) then
+    if not (csDesigning in ComponentState) then
     begin
       if not LibsamplerateLoaded then
         raise EAuException.Create(LibsampleratePath + ' library could not be loaded.');
