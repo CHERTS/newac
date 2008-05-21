a7613815af8d2e7f5b099f6c25c4bc13879a89c5
wayne@wootcomputers.com <wayne@wootcomputers.com@84581c87-d242-0410-b61f-61fc836ab3ff>
2008-05-21 00:34:19 +0000

    Updated formatting for "Suported Formats" in features.txt Included tools to build documentation archive:   filelister.exe   filelist.txt   compress_docs.bat
    It all works by commenting out "//" any files not wanted in the final archive. Automatically ignores .svn.

--- trunk/doc/html/javascript/searchdata.js
+++ trunk/doc/html/javascript/searchdata.js
@@ -1,7 +1,7 @@
 var indexSectionsWithContent = {
    "General": {
       "Symbols": true,
-      "Numbers": false,
+      "Numbers": true,
       "A": true,
       "B": true,
       "C": true,
@@ -12,7 +12,7 @@ var indexSectionsWithContent = {
       "H": true,
       "I": true,
       "J": true,
-      "K": false,
+      "K": true,
       "L": true,
       "M": true,
       "N": true,
