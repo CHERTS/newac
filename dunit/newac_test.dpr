25659492c09b5e598d0678bb92ab3cc864d09637
wayne@wootcomputers.com <wayne@wootcomputers.com@84581c87-d242-0410-b61f-61fc836ab3ff>
2008-07-05 02:26:27 +0000

    Updated comments

--- dunit/newac_test.dpr
+++ dunit/newac_test.dpr
@@ -1,5 +1,27 @@
 program newac_test;
 
+(*
+  This file is a part of New Audio Components package v 1.6
+  Copyright (c) 2002-2007, Andrei Borovsky. All rights reserved.
+  See the LICENSE file for more details.
+  You can contact me at anb@symmetrica.net
+
+  ****************************************************************************
+*)
+
+(* $Id:$ *)
+
+(* Title: DUnit Test Project
+    This project is a collection of test code, called Test Cases, which are
+    intended to ensure code security through revisions and refactorings.
+    Currently the only focus of the tests are to compare WAVs before and after
+    transcoding with lossless formats. The sources are of various sample rates
+    and bit depths. The project should be expanded to include tests for all of
+    the various aspects of each component.
+
+    For more information on DUnit, check out http://dunit.sourceforge.net. *)
+
+
 {$IFDEF CONSOLE_TESTRUNNER}
 {$APPTYPE CONSOLE}
 {$ENDIF}
