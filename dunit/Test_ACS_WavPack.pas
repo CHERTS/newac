1f83c7da3a5c39883b414e33ad364c468043a30f
wayne@wootcomputers.com <wayne@wootcomputers.com@84581c87-d242-0410-b61f-61fc836ab3ff>
2008-07-05 01:54:36 +0000

    Added DUnit test project. It compiles to .\bin and requires dunit_support.rar (from files section on Google Code) extracted to .\bin. Documentation for DUnit stuff in the works.

---
+++ dunit/Test_ACS_WavPack.pas
@@ -0,0 +1,128 @@
+(*
+  This file is a part of New Audio Components package v 1.6
+  Copyright (c) 2002-2007, Andrei Borovsky. All rights reserved.
+  See the LICENSE file for more details.
+  You can contact me at anb@symmetrica.net
+
+  ****************************************************************************
+
+  TWVIn and TWVOut components are written by Sergei Borisov, <jr_ross@mail.ru>
+*)
+
+(* $Id: Test_ACS_WavPack.pas 889 2008-06-22 20:01:48Z bob $ *)
+
+(* Title: ACS_WavPack TestCases
+    This unit contains TestCases for <ACS_WavPack>. More information about DUnit
+    can be found at http://dunit.sourceforge.net. *)
+
+unit Test_ACS_WavPack;
+
+interface
+
+uses
+  uUtility,
+  uTestBase,
+
+  ACS_WavPack,
+  ACS_Classes,
+  ACS_Tags,
+  ACS_Wave,
+  WavPackDLL,
+
+  TestFramework,
+
+  Classes,
+  Dialogs,
+  SysUtils,
+  Windows;
+
+type
+
+  (* Class: TestWVDecode
+      Tests decoding of WavPack files to WAV by comparing the output to
+      reference files using CRC32. *)
+  TestWVDecode = class(TTestFileDecode)
+  strict protected
+    FWVIn: TWVIn;
+  public
+    procedure SetUp; override;
+    procedure TearDown; override;
+  published
+  (* Procedure: TestDecode
+      Decodes all WV files specified in decode_wv.txt .*)
+    procedure TestDecode; override;
+  end;
+
+  (* Class: TestWVEncode
+      Tests encoding of WAV files to WavPack by comparing the output to
+      reference files using CRC32. *)
+  TestWVEncode = class(TTestFileEncode)
+  protected
+    FWVOut: TWVOut;
+  public
+    procedure SetUp; override;
+    procedure TearDown; override;
+  published
+  (* Procedure: TestEncode
+      Encodes all WAV files specified in encode_wv.txt. *)
+    procedure TestEncode; override;
+  (* Procedure: TestEncodeHybrid
+      Encodes all WAV files specified in encode_wv.txt using the WavPack's
+      Hybrid mode. *)
+    procedure TestEncodeHyrbid;
+  end;
+
+implementation
+
+procedure TestWVDecode.SetUp;
+begin
+  inherited;
+  FWVIn := TWVIn.Create(nil);
+  FWaveOut.Input := FWVIn;
+end;
+
+procedure TestWVDecode.TearDown;
+begin
+  inherited;
+  FWVIn.Free;
+  FWVIn := nil;
+end;
+
+procedure TestWVDecode.TestDecode;
+begin
+  DecodeFiles('decode_wv.txt', FWVIn);
+end;
+
+procedure TestWVEncode.SetUp;
+begin
+  inherited;
+  FWVOut := TWVOut.Create(nil);
+  FWVOut.Input := FWaveIn;
+end;
+
+procedure TestWVEncode.TearDown;
+begin
+  FWVOut.Free;
+  FWVOut := nil;
+  inherited;
+end;
+
+procedure TestWVEncode.TestEncode;
+begin
+  EncodeFiles('encode_wv.txt', '.wv', FWVOut);
+end;
+
+procedure TestWVEncode.TestEncodeHyrbid;
+begin
+  FWVOut.HybridMode := true;
+  EncodeFiles('encode_wv.txt', ' - hybrid.wv', FWVOut);
+  Check(FilesAreIdentical(ChangeFileExt(FOutput, '.wvc'), 'media\test - 24bit 48kHz Stereo - hybrid.wvc'));
+end;
+
+initialization
+  // Register any test cases with the test runner
+  RegisterTest(TestWVDecode.Suite);
+  RegisterTest(TestWVEncode.Suite);
+
+end.
+
---
+++ dunit/uTestBase.pas
@@ -0,0 +1,188 @@
+(*
+  Title: uTestBase
+    A collection of ancestor classes for testcases.
+*)
+
+unit uTestBase;
+
+interface
+
+uses
+  uUtility,
+
+  ACS_WavPack,
+  ACS_Classes,
+  ACS_Tags,
+  ACS_Wave,
+
+  Classes,
+  SysUtils,
+  Windows,
+
+  TestFramework;
+
+
+type
+
+  (* Class: TTestFileCode
+      Ancestor class for TestCases involving transcoding from a list of
+      files. *)
+  TTestFileCode = class(TTestCase)
+  protected
+    FInput,
+      FOutput: string;
+    FInputFiles: TStringList;
+  public
+    procedure SetUp; override;
+    procedure TearDown; override;
+    procedure OnFileDone(Sender: TComponent);
+  end;
+
+  (* Class: TTestFileDecode
+      Ancestor class for TestCases using any <TAuFileIn> and decoding to WAV.
+  *)
+  TTestFileDecode = class(TTestFileCode)
+  protected
+    FWaveOut: TWaveOut;
+  public
+    procedure SetUp; override;
+    procedure TearDown; override;
+  (* Procedure: DecodeFiles
+      Opens the file specified in sListFile and uses aufInput to decode the
+      listed files to temp\<filename>, changing their extension to '.wav'.
+      Please note that the resulting WAV headers will *not* be of the Microsoft
+      format (ie. CreateMSHeaders = false). This is opposite to the default. *)
+    procedure DecodeFiles(sListFile: string; aufInput: TauFileIn);
+  published
+    procedure TestDecode; virtual; abstract;
+  end;
+
+  (* Class: TTestFileEncode
+      Ancestor class for TestCases using any <TAuFileOut> and encoding from WAV.
+  *)
+  TTestFileEncode = class(TTestFileCode)
+  protected
+    FWaveIn: TWaveIn;
+  public
+    procedure SetUp; override;
+    procedure TearDown; override;
+  (* Procedure: EncodeFiles
+      Opens the file specified in sListFiles and uses aufOutput to
+      encode the listed files to temp\<filename>, changing their extention to
+      sExt. *)
+    procedure EncodeFiles(sListFile, sExt: string; aufOutput: TauFileOut);
+  published
+    procedure TestEncode; virtual; abstract;
+  end;
+
+implementation
+
+{ TTestInput }
+
+procedure TTestFileDecode.DecodeFiles(sListFile: string; aufInput: TauFileIn);
+const
+  sHybridExt = ' - hybrid';
+var
+  i, x: integer;
+begin
+  FInputFiles.LoadFromFile(sListFile);
+  for i := 0 to Pred(FInputFiles.Count) do
+  begin
+    FInput := FInputFiles[i];
+    x := Pos(sHybridExt, FInput);
+    if x = 0 then
+      FOutput := 'temp\' + ChangeFileExt(ExtractFileName(FInputFiles[i]), '.wav')
+    else
+    begin
+      FOutput := Copy(FInput, 1, Pred(x)) + Copy(FInput, x + Length(sHybridExt), MAXINT);
+      FOutput := 'temp\' + ChangeFileExt(ExtractFileName(FOutput), '.wav')
+    end;
+    Assert(FileExists(FInput));
+    aufInput.FileName := FInput;
+    FWaveOut.FileName := FOutput;
+    FWaveOut.BlockingRun;
+    OnFileDone(FWaveOut);
+  end;
+end;
+
+procedure TTestFileDecode.SetUp;
+begin
+  inherited;
+  FWaveOut := TWaveOut.Create(nil);
+  FWaveOut.WavType := wtPCM;
+  FWaveOut.CreateNonMsHeaders := true;
+end;
+
+procedure TTestFileDecode.TearDown;
+begin
+  FWaveOut.Free;
+  FWaveOut := nil;
+  inherited;
+end;
+
+
+{ TTestCode }
+
+procedure TTestFileCode.OnFileDone(Sender: TComponent);
+var
+  aufInput: TAuFileIn;
+  aufOutput: TAuFileOut;
+  sReference: string;
+begin
+  aufOutput := Sender as TAuFileOut;
+  aufInput := aufOutput.Input as TAuFileIn;
+  sReference := ExtractFilePath(aufInput.FileName) + ExtractFileName(aufOutput.FileName);
+  Check(FilesAreIdentical(sReference, FOutput), 'File Mismatch: ' + sReference);
+end;
+
+procedure TTestFileCode.SetUp;
+begin
+  inherited;
+  FInputFiles := TStringList.Create;
+end;
+
+procedure TTestFileCode.TearDown;
+var
+  i: Integer;
+begin
+//co-opt FInputFiles for file deleting, comment out the FindFiles line to generate references
+  FInputFiles.Clear;
+ // FindFiles(FInputFiles, 'temp\', '*.*');
+  for i := 0 to Pred(FInputFiles.Count) do
+    DeleteFile(PChar(FInputFiles[i]));
+  FInputFiles.Destroy;
+end;
+
+{ TTestEncode }
+
+procedure TTestFileEncode.EncodeFiles(sListFile, sExt: string;
+  aufOutput: TauFileOut);
+var
+  i: integer;
+begin
+  FInputFiles.LoadFromFile(sListFile);
+  for i := 0 to Pred(FInputFiles.Count) do
+  begin
+    FInput := FInputFiles[i];
+    FOutput := 'temp\' + ChangeFileExt(ExtractFileName(FInputFiles[i]), sExt);
+    Assert(FileExists(FInput));
+    FWaveIn.FileName := FInput;
+    aufOutput.FileName := FOutput;
+    aufOutput.BlockingRun;
+    OnFileDone(aufOutput);
+  end;
+end;
+
+procedure TTestFileEncode.SetUp;
+begin
+  inherited;
+  FWaveIn := TWaveIn.Create(nil);
+end;
+
+procedure TTestFileEncode.TearDown;
+begin
+  FWaveIn.Destroy;
+  inherited;
+end;
+
+end.
